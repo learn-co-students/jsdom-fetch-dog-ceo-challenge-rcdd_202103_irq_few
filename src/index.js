c9bd478cdd9d5231b9a59bb70d03a33d2199fefb
Avraz  Zebary <avrazebary@gmail.com>
2021-04-26 14:13:04 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+fetch(imgUrl).then(response => response.json()).then(data => console.log(data))
+
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+fetch(breedUrl).then(response => response.json()).then(data => console.log(data))
