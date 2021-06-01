9ab1f196b7678a4be00180efebb112319e873d5f
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-06-01 06:51:52 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 let breeds = [];
 
 document.addEventListener('DOMContentLoaded', function () {
-  loadImages();
-  loadBreedOptions();
+  fetchDogImage();
+  fetchBreeds();
 });
 
-function loadImages() {
+function fetchDogImage() {
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
   fetch(imgUrl)
     .then(res=> res.json())
@@ -21,7 +21,7 @@ function addImage(dogPicUrl) {
   container.appendChild(newImageEl);
 }
 
-function loadBreedOptions() {
+function fetchBreeds() {
   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
   fetch(breedUrl)
     .then(res => res.json())
@@ -59,8 +59,8 @@ function addBreedSelectListener() {
 }
 
 function addBreed(breed) {
-  let ul = document.querySelector('#dog-breeds');
-  let li = document.createElement('li');
+  const ul = document.querySelector('#dog-breeds');
+  const li = document.createElement('li');
   li.innerText = breed;
   li.style.cursor = 'pointer';
   ul.appendChild(li);
@@ -68,5 +68,5 @@ function addBreed(breed) {
 }
 
 function updateColor(event) {
-  event.target.style.color = 'palevioletred';
+  event.target.style.color = 'skyblue';
 }
